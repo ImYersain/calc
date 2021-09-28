0ebc7e15f78924ebfc35f09364fba5ea9cb7d9da
Yersain <71192180+ImYersain@users.noreply.github.com>
2021-09-28 22:19:02 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const   inputKC = document.querySelector("#kc"),
+        inputUSD = document.querySelector("#usd");
+
+inputKC.addEventListener('input', ()=>{
+    const request = new XMLHttpRequest();
+    request.open('GET','js/current.json');
+    request.setRequestHeader('Content-type','application/json; charset=utf-8');
+    request.send(); 
+
+    request.addEventListener('load',()=>{
+        if(request.status === 200 ) {
+            const data = JSON.parse(request.response);
+            inputUSD.value = (+inputKC.value / data.current.usd).toFixed(2);
+        } else {
+            inputUSD.value = "error 404";
+        }
+         
+    });
+});
